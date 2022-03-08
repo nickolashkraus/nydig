c02d3e376dc263792f9cd0362513dc9bdcc373a2
NickolasHKraus <NickHKraus@gmail.com>
2022-03-08 10:11:12 -0600

    Simplify s3-backend module layout

--- infrastructure/core/s3-backend/deploy/infrastructure-production/main.tf
+++ infrastructure/core/s3-backend/deploy/infrastructure-production/main.tf
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# AWS ORGANIZATIONS
+# AMAZON S3 BUCKET AND DYNAMODB TABLE FOR STORING TERRAFORM STATE
 # Deploy 's3_backend' Terraform module to 'infrastructure-production' AWS
 # account.
 # -----------------------------------------------------------------------------
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 module "s3_backend" {
-  source         = "../../modules/s3-backend"
+  source         = "../../"
   s3_bucket      = "terraform-state-nydig"
   dynamodb_table = "terraform-state-nydig"
 }
