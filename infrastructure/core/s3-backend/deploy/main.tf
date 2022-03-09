0a5f10682aff30dff63b2491be09b708c2f03299
NickolasHKraus <NickHKraus@gmail.com>
2022-03-09 10:26:52 -0600

    Fix typo

--- infrastructure/core/s3-backend/deploy/main.tf
+++ infrastructure/core/s3-backend/deploy/main.tf
@@ -3,10 +3,6 @@
 # Deploy 's3_backend' Terraform module to 'infrastructure-production' AWS
 # account.
 # -----------------------------------------------------------------------------
-
-# -----------------------------------------------------------------------------
-# AMAZON S3 BUCKET AND DYNAMODB TABLE FOR STORING TERRAFORM STATE
-# -----------------------------------------------------------------------------
 provider "aws" {
   region = "us-east-1"
 }
